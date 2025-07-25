08c03c8fba7fe78acb192ff89dd5e83f2ef06aa8
poybro <onquoctuan@gmail.com>
2025-07-25 14:51:49 +0700

    Delete sok directory

--- sok/__init__.py
+++
@@ -1,7 +0,0 @@
-# sok/__init__.py
-# -*- coding: utf-8 -*-
-
-from .blockchain import Blockchain
-from .transaction import Transaction
-from .wallet import Wallet, sign_data, verify_signature
-from .utils import hash_data, Config
